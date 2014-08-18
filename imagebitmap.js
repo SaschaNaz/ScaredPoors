64f75b554d349395a5a90f4af0c9c44e206fe7c3
SaschaNaz <saschanaz@outlook.com>
2014-08-18 22:16:38 +0900

    createImageBitmap

--- imagebitmap.js
+++ imagebitmap.js
@@ -1,3 +1,31 @@
-﻿function createImageBitmap(image, sx, sy, width, height) {
-}
+﻿var ImageBitmap;
+(function (ImageBitmap) {
+    var canvas = document.createElement("canvas");
+    var canvasContext = canvas.getContext("2d");
+
+    function createImageBitmap(image, sx, sy, width, height) {
+        if (typeof sx === "undefined") { sx = 0; }
+        if (typeof sy === "undefined") { sy = 0; }
+        var prefix = getLengthPrefix(image);
+        var widthName = prefix ? prefix + "Width" : "width";
+        var heightName = prefix ? prefix + "Height" : "height";
+        if (width == null)
+            width = image[widthName];
+        if (height == null)
+            height = image[heightName];
+
+        canvasContext.drawImage(image, sx, sy, width, height, 0, 0, width, height);
+        return memoryBox.canvasContext.getImageData(0, 0, width, height);
+    }
+    ImageBitmap.createImageBitmap = createImageBitmap;
+
+    function getLengthPrefix(element) {
+        if (element instanceof HTMLImageElement)
+            return "natural";
+        else if (element instanceof HTMLVideoElement)
+            return "video";
+        else
+            return null;
+    }
+})(ImageBitmap || (ImageBitmap = {}));
 //# sourceMappingURL=imagebitmap.js.map
--- imagebitmap.ts
+++ imagebitmap.ts
@@ -1,2 +1,26 @@
-﻿function createImageBitmap(image: any, sx: number, sy: number, width: number, height: number) {
+﻿module ImageBitmap {
+    var canvas = document.createElement("canvas");
+    var canvasContext = canvas.getContext("2d");
+
+    export function createImageBitmap(image: any, sx = 0, sy = 0, width?: number, height?: number) {
+        var prefix = getLengthPrefix(image);
+        var widthName = prefix ? prefix + "Width" : "width";
+        var heightName = prefix ? prefix + "Height" : "height";
+        if (width == null)
+            width = image[widthName];
+        if (height == null)
+            height = image[heightName];
+
+        canvasContext.drawImage(image, sx, sy, width, height, 0, 0, width, height);
+        return memoryBox.canvasContext.getImageData(0, 0, width, height);
+    }
+
+    function getLengthPrefix(element: any) {
+        if (element instanceof HTMLImageElement)
+            return "natural";
+        else if (element instanceof HTMLVideoElement)
+            return "video";
+        else
+            return null;
+    }
 }
