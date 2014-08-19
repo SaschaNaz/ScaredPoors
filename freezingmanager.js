9e702ffae275c846c8f3e399d641ca0d3316a8ce
SaschaNaz <saschanaz@outlook.com>
2014-08-20 00:38:37 +0900

    fixing detection algorithm

--- freezingmanager.js
+++ freezingmanager.js
@@ -1,15 +1,43 @@
 ﻿var FreezingManager = (function () {
     function FreezingManager() {
         this._continuousFreezing = [];
-        this._movedLastTime = true;
+        this._wasDefreezedLastTime = true;
         this._lastStopping = null;
+        this._isFrozen = true;
+        this._latestMovements = [];
         this._totalFrozenTime = 0;
         this._elapsedTime = 0;
-        this.minimalDuration = 1.5;
+        this.observingDuration = 1.5;
     }
     Object.defineProperty(FreezingManager.prototype, "_lastStoppingDuration", {
         get: function () {
             return this._lastStopping.end - this._lastStopping.start;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(FreezingManager.prototype, "_latestMovingDuration", {
+        get: function () {
+            var duration = 0;
+            this._latestMovements.forEach(function (movement) {
+                duration += movement.judged - movement.watched;
+            });
+            return duration;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(FreezingManager.prototype, "_firstStoppingWithinDuration", {
+        get: function () {
+            var stopping = this._elapsedTime - this.observingDuration;
+            for (var i = 0; i < this._latestMovements.length; i++) {
+                var item = this._latestMovements[i];
+                if (stopping >= item.watched)
+                    stopping = item.judged; // Movement occured at that time, delaying
+                else
+                    break;
+            }
+            return stopping;
         },
         enumerable: true,
         configurable: true
@@ -19,7 +47,7 @@
             if (!this._lastStopping)
                 return 0;
             var frozen = this._totalFrozenTime;
-            if (!this._movedLastTime && this._lastStoppingDuration > this.minimalDuration)
+            if (!this._isFrozen && this._lastStoppingDuration > this.observingDuration)
                 frozen += this._lastStoppingDuration;
 
             return frozen / this._elapsedTime;
@@ -31,35 +59,65 @@
     FreezingManager.prototype.loadStopping = function (stopping) {
         this._elapsedTime = stopping.judged;
         this._presentElapsedTime(this._elapsedTime);
-        if (!stopping.isOccured) {
-            this._presentStatus("active");
-            this._movedLastTime = true;
-            return;
-        }
 
-        //occured
-        this._presentStatus("stopped");
-        if (this._movedLastTime) {
-            if (this._lastStopping) {
-                if (this._lastStoppingDuration < this.minimalDuration)
-                    this._continuousFreezing.pop();
-                else
-                    this._totalFrozenTime += this._lastStoppingDuration;
-            }
-            this._lastStopping = {
-                start: stopping.watched,
-                end: stopping.judged
-            };
+        this._clearOldMovements();
+
+        if (!stopping.isOccured)
+            this._latestMovements.push(stopping);
+
+        if (this._latestMovingDuration <= this.observingDuration * 0.1) {
+            this._lastStopping = { start: this._firstStoppingWithinDuration, end: stopping.judged };
             this._continuousFreezing.push(this._lastStopping);
-        } else {
-            this._lastStopping.end = stopping.judged;
-            if (this._lastStoppingDuration >= this.minimalDuration) {
-                this._presentStatus("frozen");
-                this._presentFrozenTime(this._totalFrozenTime + this._lastStoppingDuration);
-            }
+
+            //presenting time
+            return;
+
+            this._wasDefreezedLastTime = false;
+            this._presentStatus("frozen");
         }
 
-        this._movedLastTime = false;
+        if (!this._wasDefreezedLastTime) {
+            // fixing end time
+        }
+
+        this._wasDefreezedLastTime = true;
+        if (stopping.isOccured)
+            this._presentStatus("stopping");
+        else
+            this._presentStatus("active");
+        //if (!stopping.isOccured) {
+        //    this._presentStatus("active");
+        //    this._movedLastTime = true;
+        //    return;
+        //}
+        ////occured
+        //this._presentStatus("stopped");
+        //if (this._movedLastTime) {
+        //    if (this._lastStopping) {
+        //        if (this._lastStoppingDuration < this.minimalDuration) // too short stopping time, ignore it
+        //            this._continuousFreezing.pop();
+        //        else
+        //            this._totalFrozenTime += this._lastStoppingDuration;
+        //    }
+        //    this._lastStopping = {
+        //        start: stopping.watched,
+        //        end: stopping.judged
+        //    };
+        //    this._continuousFreezing.push(this._lastStopping);
+        //}
+        //else {
+        //    this._lastStopping.end = stopping.judged;
+        //    if (this._lastStoppingDuration >= this.minimalDuration) { // enough stopping time, continuing
+        //        this._presentStatus("frozen");
+        //        this._presentFrozenTime(this._totalFrozenTime + this._lastStoppingDuration);
+        //    }
+        //}
+        //this._movedLastTime = false;
+    };
+
+    FreezingManager.prototype._clearOldMovements = function () {
+        while (this._latestMovements[0] && this._latestMovements[0].judged <= this._elapsedTime - 1.5)
+            this._latestMovements.shift();
     };
 
     FreezingManager.prototype._presentElapsedTime = function (time) {
--- freezingmanager.ts
+++ freezingmanager.ts
@@ -1,58 +1,110 @@
 ﻿class FreezingManager {
     private _continuousFreezing: Continuity[] = [];
-    private _movedLastTime = true;
+    private _wasDefreezedLastTime = true;
     private _lastStopping: Continuity = null;
+    private _isFrozen = true;
+    private _latestMovements: Occurrence[] = [];
     private _totalFrozenTime = 0;
     private _elapsedTime = 0;
 
     private get _lastStoppingDuration() {
         return this._lastStopping.end - this._lastStopping.start;
     }
+    private get _latestMovingDuration() {
+        var duration = 0;
+        this._latestMovements.forEach((movement) => {
+            duration += movement.judged - movement.watched;
+        });
+        return duration;
+    }
+    private get _firstStoppingWithinDuration() {
+        var stopping = this._elapsedTime - this.observingDuration; // Assuming there is no movement
+        for (var i = 0; i < this._latestMovements.length; i++) {
+            var item = this._latestMovements[i];
+            if (stopping >= item.watched)
+                stopping = item.judged; // Movement occured at that time, delaying
+            else
+                break;
+        }
+        return stopping;
+    }
     get frozenRatio() {
         if (!this._lastStopping)
             return 0;
         var frozen = this._totalFrozenTime;
-        if (!this._movedLastTime && this._lastStoppingDuration > this.minimalDuration) // last stopping is not yet added while it is long enough
+        if (!this._isFrozen && this._lastStoppingDuration > this.observingDuration) // last stopping is not yet added while it is long enough
             frozen += this._lastStoppingDuration;
 
         return frozen / this._elapsedTime;
     }
 
-    minimalDuration = 1.5;
+    observingDuration = 1.5;
 
     loadStopping(stopping: Occurrence) {
         this._elapsedTime = stopping.judged;
         this._presentElapsedTime(this._elapsedTime);
-        if (!stopping.isOccured) {
-            this._presentStatus("active");
-            this._movedLastTime = true;
-            return;
-        }
-        
-        //occured
-        this._presentStatus("stopped");
-        if (this._movedLastTime) {
-            if (this._lastStopping) {
-                if (this._lastStoppingDuration < this.minimalDuration) // too short stopping time, ignore it
-                    this._continuousFreezing.pop();
-                else
-                    this._totalFrozenTime += this._lastStoppingDuration;
-            }
-            this._lastStopping = {
-                start: stopping.watched,
-                end: stopping.judged
-            };
+
+        this._clearOldMovements();
+
+        if (!stopping.isOccured)
+            this._latestMovements.push(stopping);
+
+        if (this._latestMovingDuration <= this.observingDuration * 0.1) {
+            this._lastStopping = { start: this._firstStoppingWithinDuration, end: stopping.judged };
             this._continuousFreezing.push(this._lastStopping);
-        }
-        else {
-            this._lastStopping.end = stopping.judged;
-            if (this._lastStoppingDuration >= this.minimalDuration) { // enough stopping time, continuing
-                this._presentStatus("frozen");
-                this._presentFrozenTime(this._totalFrozenTime + this._lastStoppingDuration);
-            }
+
+            //presenting time
+            return;
+
+
+            this._wasDefreezedLastTime = false;
+            this._presentStatus("frozen");
         }
 
-        this._movedLastTime = false;
+        if (!this._wasDefreezedLastTime) {
+            // fixing end time
+        }
+
+        this._wasDefreezedLastTime = true;
+        if (stopping.isOccured)
+            this._presentStatus("stopping");
+        else
+            this._presentStatus("active");
+        //if (!stopping.isOccured) {
+        //    this._presentStatus("active");
+        //    this._movedLastTime = true;
+        //    return;
+        //}
+        
+        ////occured
+        //this._presentStatus("stopped");
+        //if (this._movedLastTime) {
+        //    if (this._lastStopping) {
+        //        if (this._lastStoppingDuration < this.minimalDuration) // too short stopping time, ignore it
+        //            this._continuousFreezing.pop();
+        //        else
+        //            this._totalFrozenTime += this._lastStoppingDuration;
+        //    }
+        //    this._lastStopping = {
+        //        start: stopping.watched,
+        //        end: stopping.judged
+        //    };
+        //    this._continuousFreezing.push(this._lastStopping);
+        //}
+        //else {
+        //    this._lastStopping.end = stopping.judged;
+        //    if (this._lastStoppingDuration >= this.minimalDuration) { // enough stopping time, continuing
+        //        this._presentStatus("frozen");
+        //        this._presentFrozenTime(this._totalFrozenTime + this._lastStoppingDuration);
+        //    }
+        //}
+
+        //this._movedLastTime = false;
+    }
+
+    private _clearOldMovements() {
+        while (this._latestMovements[0] && this._latestMovements[0].judged <= this._elapsedTime - 1.5)
+            this._latestMovements.shift();
     }
 
     private _presentElapsedTime(time: number) {
